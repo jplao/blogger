f1e4905e438694f6056db9cd174ffdb415586ebc
Jenn Lao <jenn.p.lao@gmail.com>
2018-11-03 13:50:16 -0600

    adds test to see article comments

--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -4,6 +4,8 @@ describe 'user sees one article' do
   describe 'they link from articles index' do
     it 'shows information from one article' do
       article_1 = Article.create!(title: "Title 1", body: "Body 1")
+      comment_1 = article_1.comments.create(author_name: "Me", body: "Commenty comments")
+      comment_2 = article_1.comments.create(author_name: "You", body: "So much to say")
 
       visit articles_path
 
@@ -11,6 +13,10 @@ describe 'user sees one article' do
 
       expect(page).to have_content(article_1.title)
       expect(page).to have_content(article_1.body)
+      expect(page).to have_content(comment_1.author_name)
+      expect(page).to have_content(comment_1.body)
+      expect(page).to have_content(comment_2.author_name)
+      expect(page).to have_content(comment_2.body)
     end
   end
 end
